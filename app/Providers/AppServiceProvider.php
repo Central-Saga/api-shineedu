d26f2a4e17dfee6642561969cabad1228203ccda
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2026-02-09 19:59:57 +0800

    fix: handle empty google drive folder config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -46,7 +46,8 @@ class AppServiceProvider extends ServiceProvider
                 $client->refreshToken($config['refreshToken']);
 
                 $service = new \Google\Service\Drive($client);
-                $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folder'] ?? '/');
+                $folder = !empty($config['folder']) ? $config['folder'] : '/';
+                $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $folder);
                 $driver = new \League\Flysystem\Filesystem($adapter);
 
                 return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
